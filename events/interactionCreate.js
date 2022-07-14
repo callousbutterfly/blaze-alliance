0345b35d03b2412c05d6ba27dbbb808532b07f6c
callousbutterfly <callousbutterfly@gmail.com>
2022-07-14 16:54:57 -0500

    Testing the nickname feature in an interaction to allow for emphemeral messages

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,9 +2,30 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "interactionCreate",
-    run: async function runAll(bot, interaction) {
+    run: async function runAll(bot, interaction, message) {
         if(interaction.isCommand){
             const {client} = bot
+
+            if(message.channel.name === 'name-and-number'){
+                if(message.content.includes('|')){
+                    let target = message.author
+                    const member = message.guild.members.cache.get(target.id)
+                    const memUser = message.guild.members.cache.get(target)
+                    const owner = message.guild.members.cache.find(member => member.id === message.guild.ownerId);
+    
+                    //let changed = message.guild.members.filter(mem=>{mem.displayName != mem.user.username})
+    
+                    if(member !== owner && !member.displayName.includes('|')){
+                        member.setNickname(message.content)
+                        interaction.send({content:"Successfully changed your nickname, welcome to the server!", emphemeral: true})
+                    }
+                    else if(member === owner){
+                        message.reply({content: "You cannot change your user, you are the owner!", ephemeral: true})
+                    }
+                    else if(member.displayName.includes('|'))
+                        message.reply({content: "Your nickname is already changed!", ephemeral: true})
+                }
+            }
 
             if(!interaction.isCommand()) return
             if(!interaction.inGuild()) return interaction.reply("This command can only be used in a server")
