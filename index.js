72dd7eb4dac4c92bba83cbe0ae47039f753b9507
Yerti <chessboy5@icloud.com>
2022-07-14 19:43:24 -0500

    :fire: Remove old non-slash command handler

--- index.js
+++ index.js
@@ -21,10 +21,6 @@ client.slashcommands = new Discord.Collection()
 client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
 client.loadSlashCommands(bot, false);
 
-client.commands = new Discord.Collection()
-client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
-client.loadCommands(bot, false)
-
 client.events = new Discord.Collection()
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
 client.loadEvents(bot, false)
